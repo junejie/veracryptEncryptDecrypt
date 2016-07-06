aaed5454659eb46f30a4f4ec6b4652e6e3c6899a
Junejie <junejieruzol1@gmail.com>
2016-07-06 17:42:17 +0800

    base

---
+++ src/main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#######################################
+# steps
+#     encryption
+#
+#         param 
+#             1. dir to encrypt
+#
+#         1. get list of dir
+#         2. map the dir.
+#         3. save to txt file the map
+#         4. create encrypted dir match to non encrypted dir
+#
+#     decyption
+#         param
+#             1. target to decrypt
+#             2. encrypted file or dir
+#######################################
+
+
+echo test
