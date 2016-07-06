4f4e06aafc6fd74f9eeff3d823d34df7e858169c
Junejie <junejieruzol1@gmail.com>
2016-07-06 20:12:27 +0800

    change var

--- src/main.sh
+++ src/main.sh
@@ -24,33 +24,47 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 # Initialize our own variables:
 encrypt_dir=""
 verbose=0
+enc_action=""
 
-while getopts "h?vd:" opt; do
-    case "$opt" in
+while getopts "h?vedp:" opts; do
+    case "$opts" in
     h|\?)
         show_help
         exit 0
         ;;
     v)  verbose=1
         ;;
-    d)  encrypt_dir=$OPTARG
-        ls -R $encrypt_dir | awk '
-        /:$/&&f{s=$0;f=0}
-        /:$/&&!f{sub(/:$/,"");s=$0;f=1;next}
-        NF&&f{ print s"/"$0 }' > out.txt
-        echo 1
-        cat out.txt | while read line
-        do
-          echo "---"
-          echo "---$line"
-          echo "---"
-        done
+
+    # enc_action [e,d] encrypt,decrypt 
+    e)  
+        enc_action=e
+        ;;
+    d)  
+        enc_action=d
+        ;;
+    p)  
+        encrypt_dir=$OPTARG
         ;;
     esac
 done
+
+### start process ###
+echo $enc_action
+ls -R $encrypt_dir | awk '
+/:$/&&f{s=$0;f=0}
+/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}
+NF&&f{ print s"/"$0 }' > out.txt
+echo 1
+cat out.txt | while read line
+    do
+      echo "---"
+    done
+
+### end proc ###
+
 
 shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-echo "verbose=$verbose, encrypt_dir='$encrypt_dir', Leftovers: $@"
+echo "verbose=$verbose,enc_action=$enc_action, encrypt_dir='$encrypt_dir', Leftovers: $@"
