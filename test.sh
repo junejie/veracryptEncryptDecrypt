14631f4136073552661a56711810302cf8b30453
Junejie <junejieruzol1@gmail.com>
2016-07-08 02:48:10 +0800

    add test

--- test.sh
+++ test.sh
@@ -1,29 +1,16 @@
-execute: 
-    encrypt
-        sudo sh src/main.sh -v -p test -e -o output -a "abc123"
+#!/bin/bash
 
+sudo sh src/main.sh -v -p test -e -o output -a "abc123"
+sudo sh src/main.sh -v -p output -d -o test -a "abc123"
 
-    decrypt
-        sudo sh src/main.sh -v -p output -d -o test -a "abc123"
-
-
-how to execute the test validity
-    sudo sh src/main.sh -v -p test -e -o output -a "abc123"
-    sudo sh src/main.sh -v -p output -d -o testx -a "abc123"
-
-    there should have no result 
-        diff --brief -Nr  test testx/output/test
-
-    the hash should be the same
-        
-        { export LC_ALL=C;cd test;
+{ export LC_ALL=C;cd test;
           du -0ab | sort -z; # file lengths, including directories (with length 0)
           echo | tr '\n' '\000'; # separator
           find -type f -exec sha256sum {} + | sort -z; # file hashes
           echo | tr '\n' '\000'; # separator
           echo "End of hashed data."; # End of input marker
         } | sha256sum 
-        { export LC_ALL=C;cd testx/output/test;
+{ export LC_ALL=C;cd testx/output/test;
           du -0ab | sort -z; # file lengths, including directories (with length 0)
           echo | tr '\n' '\000'; # separator
           find -type f -exec sha256sum {} + | sort -z; # file hashes
