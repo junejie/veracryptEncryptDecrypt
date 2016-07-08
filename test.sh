d6e33c64858b2f73a704220df62d93d6c0c291dd
Junejie <junejieruzol1@gmail.com>
2016-07-08 12:53:33 +0800

    udpate test

--- test.sh
+++ test.sh
@@ -3,19 +3,27 @@
 sudo sh src/main.sh -v -p simpletest -e -o output -a "abc123"
 sudo sh src/main.sh -v -p output -d -o simpletestx -a "abc123"
 
-{ export LC_ALL=C;cd simpletest;
+test1={ export LC_ALL=C;cd simpletest;
           du -0ab | sort -z; # file lengths, including directories (with length 0)
           echo | tr '\n' '\000'; # separator
           find -type f -exec sha256sum {} + | sort -z; # file hashes
           echo | tr '\n' '\000'; # separator
           echo "End of hashed data."; # End of input marker
         } | sha256sum 
-{ export LC_ALL=C;cd simpletestx/output/simpletest;
+test2={ export LC_ALL=C;cd simpletestx/output/simpletest;
           du -0ab | sort -z; # file lengths, including directories (with length 0)
           echo | tr '\n' '\000'; # separator
           find -type f -exec sha256sum {} + | sort -z; # file hashes
           echo | tr '\n' '\000'; # separator
           echo "End of hashed data."; # End of input marker
         } | sha256sum
+
+
+
+if [ "$test1" = "$test2" ]; then
+  echo 'ok: simpletest'
+else
+  echo 'fail: simpletest'
+fi
 
 sudo rm simpletestx -rf
